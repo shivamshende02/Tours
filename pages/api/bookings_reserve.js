cc75f0f9987906cacec06ca5b77fc3c313f3248e
shivamshende02 <shivamshende1109@gmail.com>
2026-08-04 11:49:32 +0530

    added @ in few paths

--- pages/api/bookings_reserve.js
+++ pages/api/bookings_reserve.js
@@ -1,5 +1,5 @@
 // pages/api/bookings/reserve.js
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "@lib/supabaseClient";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
--- pages/api/inquiries.js
+++ pages/api/inquiries.js
@@ -1,5 +1,5 @@
 // pages/api/inquiries/index.js
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "@lib/supabaseClient";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
